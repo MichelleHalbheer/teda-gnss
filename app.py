08bc2f2cc336535c0c11dfc17acab29032830c80
Mike Halbheer <mike@halbheer.ch>
2021-12-16 08:10:09 +0100

    App started

---
+++ app.py
@@ -0,0 +1,10 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+
+class TEDAGNSS(MDApp):
+    def build(self):
+        screen = MDScreen()
+
+        return screen
+
+TEDAGNSS().run()
