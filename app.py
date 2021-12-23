9fc61b571fd49b971894bb0d29afdfff80edf7b2
Mike Halbheer <mike@halbheer.ch>
2021-12-23 10:19:19 +0100

    Working prototpye

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
+from logging import error
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.tab import MDTabsBase
 from kivymd.icon_definitions import md_icons
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.uix.textfield import MDTextField
 from kivymd.toast import toast
 from kivy.factory import Factory
-from kivy.uix.modalview import ModalView
 from kivy.core.window import Window
 from kivymd.uix.filemanager import MDFileManager
-from kivymd.theming import ThemeManager
 from kivy.clock import Clock
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 from kivymd.uix.picker import MDDatePicker
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 
 from datetime import datetime
 import sys
 import json
 import os
+import re
 
 from file_handler import ReachHandler
 
@@ -58,10 +58,16 @@ class TEDAGNSS(MDApp):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._point_name = None
+        self._antenna_height = None
         self._file_path = None
-        self.file_name = None
+        self._file_name = None
         self._obs_date = None
         self._recording_date = None
+
+        f = open(os.path.join(os.path.dirname(__file__), 'config_template.json'))
+        self._config = json.load(f)
+        f.close()
 
         Window.bind(on_keyboard=self.events)
         self.manager_open = False
@@ -70,6 +76,18 @@ class TEDAGNSS(MDApp):
             select_path=self.select_path,
             ext=['.zip']
         )
+
+        self.error_dialog = MDDialog(
+                text='',
+                buttons=[
+                    MDFlatButton(
+                        text="SCHLIESSEN",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_release=self.dismiss_error_dialog
+                    )
+                ],
+            )
 
     def build(self):
         self.theme_cls.primary_palette = "Blue"
@@ -119,10 +137,54 @@ class TEDAGNSS(MDApp):
     #     date_dialog.open()
 
     def parse_file(self):
-        if not self._file_path:
-            pass
+        error_list = []
+        if self.root.current_screen.ids.point_name.text:
+            self._point_name = self.root.current_screen.ids.point_name.text
+        else:
+            error_list.append('einen Punktnamen')
+        
+        if self.root.current_screen.ids.antenna_height.text:
+            try:
+                self._antenna_height = float(self.root.current_screen.ids.antenna_height.text)
+            except ValueError:
+                error_list.append('eine gültige Antennenhöhe')
+        else:
+            error_list.append('eine gültige Antennenhöhe')
+            
+        if self.root.current_screen.ids.observation_date.text:
+            match = re.match(
+                r"^([0-9]{4})-?(1[0-2]|0[1-9])-?(3[01]|0[1-9]|[12][0-9])$",
+                self.root.current_screen.ids.observation_date.text
+            )
+            if match:
+                try:
+                    self._obs_date = datetime.strptime(
+                        self.root.current_screen.ids.observation_date.text,
+                        '%Y-%m-%d'    
+                    )
+                except ValueError:
+                    error_list.append('ein gültiges Beobachtungsdatum')
+            else:
+                error_list.append('ein gültiges Beobachtungsdatum')
+        else:
+                error_list.append('ein gültiges Beobachtungsdatum')
 
-        self._obs_date = datetime.strptime(self._file_name[9:17],'%Y%m%d') 
+        if not error_list and self._file_path:
+            handler = ReachHandler(name=self._point_name)
+            handler.parse_file(self._file_path, self._config, self._obs_date)
+
+        else:
+            self.show_error_dialog(error_list)
+
+    def show_error_dialog(self, error_list):
+        file_path_text = ' und wähle ein Beobachtungsfile aus' if not self._file_path else ''
+        error_text = f'Gib {", ".join(error_list)} ein{file_path_text}.'
+        self.error_dialog.text = error_text
+        self.error_dialog.open()
+
+    def dismiss_error_dialog(self, *args):
+        self.error_dialog.dismiss()
+
 
 def main(config_file):
     root = os.path.dirname(__file__)
